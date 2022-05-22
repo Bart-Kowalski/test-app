6cb4ce1d1868f46bba21684873e9671071f11342
DagothKur <bartosz@mystform.com>
2022-05-22 21:11:01 +0200

    feat(newForm): add form component:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {InputForm} from "./InputForm";
 
 function App() {
 
-  const [counter, setCounter] = useState<number>(0)
-
-  const handleShowAlert = (value: number) => () => {
-    alert("Hello React")
-    setCounter(counter + 1);
-  }
-
-  useEffect(() => {
-    setCounter(0);
-  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>{`Alert was shown: ${counter} times`}</div>
-        <button onClick={handleShowAlert(0)}>Show alert</button>
+        <InputForm defaultValue={'Hello form'}/>
       </header>
     </div>
   );
