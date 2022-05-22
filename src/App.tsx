3b0cf01d1298a640566166008ce65b4c3e88291b
DagothKur <bartosz@mystform.com>
2022-05-22 20:35:05 +0200

    feat(newForm): add form component:

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState<number>(0)
+
+  const handleShowAlert = (value: number) => () => {
+    alert("Hello React")
+    setCounter(counter + 1);
+  }
+
+  useEffect(() => {
+    setCounter(0);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{`Alert was shown: ${counter} times`}</div>
+        <button onClick={handleShowAlert(0)}>Show alert</button>
       </header>
     </div>
   );
